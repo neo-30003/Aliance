9c6ba0a296c6144c5ce6d41e37b939b6e73ff7cb
neo-30003 <dr.arrum@gmail.com>
2024-02-29 23:19:53 +0300

    add: sliders for mobile devices

--- js/main.js
+++ js/main.js
@@ -33,3 +33,54 @@ mMenuToggle.addEventListener("click",(event) =>{
   event.preventDefault();
   menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
+
+const swiper = new Swiper(".header-features-slider", {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+})
