362575287a56cdcae579f806cfcf7b98be5d86ff
neo-30003 <dr.arrum@gmail.com>
2024-03-16 16:01:53 +0300

    add: modal window

--- js/main.js
+++ js/main.js
@@ -102,3 +102,23 @@ const swiperBlog = new Swiper(".blog-slider", {
     },
   },
 })
+
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) && 
+     modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
+});
