66456a7a628af17a0d68c34e44885d7308fe395e
neo-30003 <dr.arrum@gmail.com>
2024-04-15 01:14:25 +0300

    add: breadcrumbs for avtohim and blog pages

---
+++ template-parts/breadcrumbs.php
@@ -0,0 +1,16 @@
+<header class="page-header <?= $header_style;?>">
+  <div class="container">
+    <div class="seporator"></div>
+    <h1 class="page-header-title"><?= $page_title ?></h1>
+    <ul class="breadcrumbs">
+      <li class="breadcrumbs-item">
+        <a href="./index.php" class="breadcrumbs-link">Главная</a>
+      </li>
+      <li class="breadcrumbs-item active">
+        <a href="#" class="breadcrumbs-link"><?= $page_title ?></a>
+      </li>
+    </ul>
+    <!-- /.breadcrumbs -->
+  </div>
+  <!-- /.container -->
+</header>
